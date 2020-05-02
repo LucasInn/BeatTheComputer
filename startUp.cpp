4117a397dce601b1a807f3ff1266637485bebbf7
LucasInn <60215520+LucasInn@users.noreply.github.com>
2020-05-02 20:27:18 +0800

    primeOrNot.cpp
    A very old-fashioned way of beating the computer. Prime-Player wins; AnythingElse-Player loses.
    if lost, move on to the next round(still to be updated)
    if won, stay.

--- startUp.cpp
+++ startUp.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+int main(){
+	srand(time(NULL));
+    int computer=0;
+    computer=rand();
+	if(computer % 2 == 0 || computer % 3 == 0 || computer % 5 == 0 || computer % 7 == 0 || computer % 9 == 0 || computer % 11 == 0){
+		cout<<"Player loses"<<endl;
+		
+	}else{
+		cout<<"Player wins"<<endl;
+		
+	}
+	
 
+    return 0;
+}
